7bbbdcd7d53b449120b991757f17ce760cb6e5de
Youngho Jeong <batsu05@gmail.com>
2015-05-08 15:21:31 +0900

    integrating appnext native sdk

--- app/src/main/java/com/brandon/brandonadnetworktest/AppnextActivity.java
+++ app/src/main/java/com/brandon/brandonadnetworktest/AppnextActivity.java
@@ -5,8 +5,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.appnext.appnextsdk.API.AppnextAPI;
+import com.appnext.appnextsdk.API.AppnextAd;
 import com.appnext.appnextsdk.API.AppnextAdRequest;
 import com.appnext.appnextsdk.Appnext;
 import com.appnext.appnextsdk.NoAdsInterface;
@@ -17,58 +22,96 @@ import java.util.ArrayList;
 
 public class AppnextActivity extends ActionBarActivity {
 
-    AppnextAPI api;
-    Appnext appnext;
+    AppnextAPI api = null;
+    AppnextAd ad = null;
+//    Appnext appnext;
+
+    ImageView ivIcon = null;
+    TextView tvTitle = null;
+    TextView tvText = null;
+    TextView tvUrl = null;
+    Button btnCta = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appnext);
 
+        ivIcon = (ImageView)findViewById(R.id.native_icon_image);
+        tvTitle = (TextView)findViewById(R.id.native_title);
+        tvText = (TextView)findViewById(R.id.native_text);
+        tvUrl = (TextView)findViewById(R.id.native_url);
+        btnCta = (Button)findViewById(R.id.native_cta);
+
 //        AppnextAPI api;
 //        Appnext appnext;
 
-        appnext = new Appnext(this);
-        appnext.setAppID("05390be9-7da9-402d-b9cd-67bbb53fe5e9"); // Set your AppID
-        appnext.showBubble(); // show the interstitial
-        appnext.setAdLoadInterface(new OnAdLoadInterface() {
-            @Override
-            public void adLoaded() {
-                Log.v("appnext", "on ad load");
-            }
-        });
-        appnext.setNoAdsInterface(new NoAdsInterface() {
-            @Override
-            public void noAds() {
-                Log.v("appnext", "no ads");
-            }
-        });
+        // interstitial ads
+//        appnext = new Appnext(this);
+//        appnext.setAppID("05390be9-7da9-402d-b9cd-67bbb53fe5e9"); // Set your AppID
+//        appnext.showBubble(); // show the interstitial
+//        appnext.setAdLoadInterface(new OnAdLoadInterface() {
+//            @Override
+//            public void adLoaded() {
+//                Log.v("appnext", "on ad load");
+//            }
+//        });
+//        appnext.setNoAdsInterface(new NoAdsInterface() {
+//            @Override
+//            public void noAds() {
+//                Log.v("appnext", "no ads");
+//            }
+//        });
 
+        // native ads
         api = new AppnextAPI(this, "05390be9-7da9-402d-b9cd-67bbb53fe5e9");
         api.setAdListener(new AppnextAPI.AppnextAdListener() {
             @Override
             public void onError(String error) {
+                Log.v("appnext", "on error");
             }
 
             @Override
             public void onAdsLoaded(ArrayList ads) {
                 //collect and store the data received
+                Log.v("appnext", "on ad loaded : " + ads.size());
 
+
+
+                if(ads.size() > 0) {
+                    ad = (AppnextAd) ads.get(0);
+//                    api.adClicked(ad);
+
+                    tvTitle.setText(ad.getAdTitle());
+                    tvText.setText(ad.getAdDescription());
+                    tvUrl.setText(ad.getAdPackage());
+                    new DownloadImageTask((ImageView) findViewById(R.id.native_icon_image))
+                            .execute(ad.getImageURL());
+
+                    btnCta.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            api.adClicked(ad);
+                        }
+                    });
+
+                }
 
             }
         });
         api.loadAds(new AppnextAdRequest());
     }
 
-    @Override
-    public void onBackPressed() {
-        if(appnext.isBubbleVisible()){
-            appnext.hideBubble();
-        }
-        else{
-            super.onBackPressed();
-        }
-    }
+//    @Override
+//    public void onBackPressed() {
+////        if(appnext.isBubbleVisible()){
+////            appnext.hideBubble();
+////        }
+////        else{
+////            super.onBackPressed();
+////        }
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
